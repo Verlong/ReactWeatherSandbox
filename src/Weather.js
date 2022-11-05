da5be46f4adf87129cf57dc445b516840ef158f5
Maryna <bonko23@gmail.com>
2022-11-05 16:44:19 +0000

    Update 2 files

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 export default function Weather(props) {
-  function fahrenheitTemperature() {
+  function showFarehrenheit(event) {
+    event.preventDefault();
     let temperature = (props.temperature * 9) / 5 + 32;
-    return Math.round(temperature);
-  }
 
+    alert(`The temperature in farehrenheit is ${Math.round(temperature)}°F`);
+  }
   return (
     <p>
-      The weather in {props.city} is {props.temperature}°C and{" "}
-      {fahrenheitTemperature()}°F
+      The weather in {props.city} is {props.temperature}°C |{" "}
+      <a href="/" onClick={showFarehrenheit}>
+        °F
+      </a>
     </p>
   );
 }
