07bec3cebe21b0d37372086a53e93cbe03e707cb
Maryna <bonko23@gmail.com>
2022-11-06 22:51:22 +0000

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Weather(props) {
+  let [temperature, setTemperature] = useState(props.temperature);
+  let [unit, setUnit] = useState("Metric");
+
   function showFarehrenheit(event) {
     event.preventDefault();
-    let temperature = (props.temperature * 9) / 5 + 32;
-
-    alert(`The temperature in farehrenheit is ${Math.round(temperature)}°F`);
+    setUnit("Imperial");
+    setTemperature(Math.round((props.temperature * 9) / 5 + 32));
+    //let temperature = (props.temperature * 9) / 5 + 32;
+    // alert(`The temperature in farehrenheit is ${Math.round(temperature)}°F`);
   }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("Metric");
+    setTemperature(props.temperature);
+  }
+
   return (
     <p>
-      The weather in {props.city} is {props.temperature}°C |{" "}
+      Weather in {props.city} is {temperature} ({unit}){" "}
+      <a href="/" onClick={showCelsius}>
+        °C
+      </a>
+      |{" "}
       <a href="/" onClick={showFarehrenheit}>
         °F
       </a>
